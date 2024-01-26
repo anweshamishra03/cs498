b746140a931fbe0de43139f7ae00e23fe549f03d
anweshamishra03 <90567203+anweshamishra03@users.noreply.github.com>
2024-01-26 14:51:12 -0600

    Update serve.py

--- serve.py
+++ serve.py
@@ -6,17 +6,14 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_private_ip():
-    # Get and return the private IP address
     hostname = socket.gethostname()
     private_ip = socket.gethostbyname(hostname)
     return private_ip
 
 @app.route('/', methods=['POST'])
-def run_stress_cpu():
-    # Run stress_cpu.py in a separate process
+def stress_cpu():
     subprocess.Popen(["python3", "stress_cpu.py"])
-    return "CPU stress process started", 202
+    return "CPU stress test initiated", 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
-
+    app.run(host='0.0.0.0', port=8080)
